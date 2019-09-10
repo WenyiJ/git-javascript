f4fc7b7f45f4d29ad95bfdd6bc15c20036da7b3b
Wenyi Jiang <jian0099@algonquinlive.com>
2019-09-10 12:16:55 -0400

    changes calculation

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ var name = 'Wenyi Jiang'
 console.log(`Hello, my name is ${name}.
 `)
 var number = 8
-console.log(number*2)
-console.log(16*5)
-console.log(80/number)
-console.log(10-7)
+console.log(number * 2 * 5 / number - 7)
+
 
 
